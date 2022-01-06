08d81947bad1f8e297f76acc39874f38ea351f98
ShinHan <tlsgks8668@naver.com>
2022-01-07 00:35:16 +0900

    feat: 예제 프로젝트 생성

---
+++ advanced/src/main/java/hello/advanced/config/LogTraceConfig.java
@@ -0,0 +1,15 @@
+package hello.advanced.config;
+
+import hello.advanced.trace.logtrace.FieldLogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LogTraceConfig {
+
+    @Bean
+    public ThreadLocalLogTrace logTrace() {
+        return new ThreadLocalLogTrace();
+    }
+}
